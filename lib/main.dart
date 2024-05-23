ae6a4d6af9030aa5aa44842cbe3f31f6a22d3690
Celine Martha Sari <gorenglahayam@gmail.com>
2024-05-23 15:53:59 +0700

    fix runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mittens/pages/home/index.dart';
 import 'package:mittens/pages/login/index.dart';
 import 'package:mittens/service/grpc_service.dart';
 
 void main() {
   runApp(const MyLoginPage());
-  // runApp(const MyHomeScreen());
 
   GrpcService().initAsync();
 }
